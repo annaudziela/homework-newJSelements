ceb7648d769a6c6661adb0fd8e6fb489b3f382df
Anna Udziela <annaudziela@gmail.com>
2021-03-06 17:03:46 +0100

    properties for listElement set

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 console.log('helloooo')
+
+const listElement = document.createElement("li");
+listElement.className = "list-item";
+listElement.id = "listItem";
+listElement.innerText = "I am one of the items on the list";
+
+console.log(listElement);
