0a4962cf18b16f40b31620369452adc9609ca883
Anna Udziela <annaudziela@gmail.com>
2021-03-08 15:25:36 +0100

    new functionality added - input values create new list

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 
 const addButton = document.querySelector(".add-button");
+const inputButton = document.querySelector(".input-button");
+const input = document.querySelector(".input");
+
+
 let counter = 0;
 
 addButton.addEventListener('click', () => {
 
-const listElement = document.createElement("li");
-listElement.className = "list-item";
-listElement.id = "listItem";
-listElement.innerText = `I am the ${++counter} item on the list`;
-const list = document.querySelector('.list');
-list.appendChild(listElement);
+    const listElement = document.createElement("li");
+    listElement.className = "list-item";
+    listElement.id = "listItem";
+    listElement.innerText = `I am the ${++counter} item on the list`;
+    const list = document.querySelector('.list');
+    list.appendChild(listElement);
+});
+
+inputButton.addEventListener('click', () => {
+    const myIdea = input.value;
+    const list = document.querySelector('.list');
+    list.append(`${myIdea}`);
 });
